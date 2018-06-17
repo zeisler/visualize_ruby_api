a35aae082f838352990c7b691689a93876d1725f
Dustin Zeisler <Dustin@Zeisler.net>
2018-06-17 16:19:16 -0700

    Update visualize_ruby gem Add error handling

--- app/controllers/visualize_ruby_controller.rb
+++ app/controllers/visualize_ruby_controller.rb
@@ -3,10 +3,12 @@ require "visualize_ruby"
 class VisualizeRubyController < ApplicationController
   def show
     svg = VisualizeRuby::Graphviz.new(
-        *VisualizeRuby::Parser.new(ruby_code).parse
+        *VisualizeRuby::Builder.new(ruby_code: ruby_code).build
     ).to_graph(svg: String)
-    expires_in 1.hour, :public => true
+    expires_in 1.hour, public: true
     render json: { svg: svg }
+  rescue Parser::SyntaxError => e
+    render json: { exception: e.message}, status: :bad_request
   end
 
   private
